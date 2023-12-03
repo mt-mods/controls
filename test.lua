2cdcadf6d1a2e981cb4127fd2e3b2a7549593238
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2023-12-03 15:15:07 -0500

    send test data per player as to not be spam nightmare when testing with multiple players

--- test.lua
+++ test.lua
@@ -1,13 +1,16 @@
 controls.register_on_press(function(player, key)
-    minetest.chat_send_all(player:get_player_name() .. " pressed " .. key)
+    local pname = player:get_player_name()
+    minetest.chat_send_player(pname, pname .. " pressed " .. key)
 end)
 
 controls.register_on_hold(function(player, key, length)
-    minetest.chat_send_all(player:get_player_name() .. " held " .. key .. " for " .. length .. " seconds")
+    local pname = player:get_player_name()
+    minetest.chat_send_player(pname, pname .. " held " .. key .. " for " .. length .. " seconds")
 end)
 
 controls.register_on_release(function(player, key, length)
-    minetest.chat_send_all(player:get_player_name() .. " released " .. key .. " after " .. length .. " seconds")
+    local pname = player:get_player_name()
+    minetest.chat_send_player(pname, pname .. " released " .. key .. " after " .. length .. " seconds")
 end)
 
 minetest.register_on_joinplayer(function(player, _)
