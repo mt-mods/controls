ba2402c056b6134dd61b1b8d8deb2971ad5dac49
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2023-12-03 14:51:55 -0500

    store callbacks in the traditional minetest way, expand tests

--- init.lua
+++ init.lua
@@ -1,19 +1,25 @@
 controls = {
+    --util values
     modpath = minetest.get_modpath("controls"),
-    testsmode = minetest.settings:get_bool("controls_enable_tests", false)
+    testsmode = minetest.settings:get_bool("controls_enable_tests", false),
+
+    --location to store callbacks
+    registered_on_press = {},
+    registered_on_hold = {},
+    registered_on_release = {},
 }
 
 --api functions
 function controls.register_on_press(callback)
-
+    table.insert(controls.registered_on_press, callback)
 end
 
 function controls.register_on_hold(callback)
-
+    table.insert(controls.registered_on_hold, callback)
 end
 
 function controls.register_on_release(callback)
-
+    table.insert(controls.registered_on_release, callback)
 end
 
 if(controls.testsmode) then
